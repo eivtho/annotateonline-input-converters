a083dd967f666cfbb6afb081cf0fe7c6a221567b
Ani <vardanyan.ani28@gmail.com>
2020-03-09 18:57:12 +0400

    Complete template

--- to_sa_converters/supervisely_to_sa.py
+++ to_sa_converters/supervisely_to_sa.py
@@ -41,17 +41,64 @@ for root, dirs, files in os.walk(sv_folder, topdown=True):
         if file_name == 'meta.json':
             meta_data = json.load(open(os.path.join(root, file_name)))
 
+sa_template_loader = []
 sa_classes_loader = []
 
 for md in meta_data['classes']:
-    if md['geometry_config'] == {}:
+    point_names = []
+    if md['geometry_config'] != {}:
+        sa_template = {
+                    'type': 'template',
+                    'classId': mapped_classes_data[md['title']][0] * (-1),
+                    'probability': 100,
+                    'points': [],
+                    'connections': [],
+                    'attributes': [],
+                    'attributeNames': [],
+                    'groupId': 0,
+                    'pointLabels': {},
+                    'locked': False,
+                    'visible': True,
+                    'templateId': (mapped_classes_data[md['title']][0] * (-1)) - 1,
+                    'className': md['title'],
+                    'templateName': md['title']
+                }
+
+        for point_name in md['geometry_config']['nodes'].keys():
+            point_names.append(point_name)
+
+        for connection in md['geometry_config']['edges']:
+            index = md['geometry_config']['edges'].index(connection)
+            if connection['dst'] in point_names and connection['src'] in point_names:
+                sa_template['connections'].append(
+                    {
+                        'id': index + 1,
+                        'from': point_names.index(connection['src']) + 1,
+                        'to': point_names.index(connection['dst']) + 1
+                    }
+                )
+
+        for pn in point_names:
+            point_index = point_names.index(pn)
+            sa_template['points'].append(
+                {
+                    'id': point_index + 1,
+                    'x': md['geometry_config']['nodes'][pn]['loc'][0],
+                    'y': md['geometry_config']['nodes'][pn]['loc'][1]
+                }
+            )
+            sa_template['pointLabels'][point_index + 1] = md['geometry_config']['nodes'][pn]['label']
+        sa_template_loader.append(sa_template)
+
+    else:
         classes_dict = {
             'name': md['title'],
-            'id': mapped_classes_data[md['title']][0],
+            'id':  mapped_classes_data[md['title']][0] * (-1),
             'color': md['color'],
             'attribute_groups': []
         }
 
         sa_classes_loader.append(classes_dict)
+
 with open(os.path.join(classes_dir, "classes.json"), "w") as classes_json:
     json.dump(sa_classes_loader, classes_json, indent=2)

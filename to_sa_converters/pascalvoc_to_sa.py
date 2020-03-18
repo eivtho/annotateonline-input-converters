8df768bec3ea8604c8e775294f3b8c63bf59e009
Ani <vardanyan.ani28@gmail.com>
2020-03-18 18:32:51 +0400

    Updated attributes

--- to_sa_converters/pascalvoc_to_sa.py
+++ to_sa_converters/pascalvoc_to_sa.py
@@ -30,6 +30,11 @@ for root, dirs, files in os.walk(pvoc_folder, topdown=True):
         if file_name.endswith('xml'):
             xml = file_name
             pvoc_json = eval(json.dumps(xmltodict.parse(open(os.path.join(root, file_name)).read())))
+
+            obj_list = []
+            if not isinstance(pvoc_json['annotation']['object'], list):
+                obj_list.append(pvoc_json['annotation']['object'])
+                pvoc_json['annotation']['object'] = obj_list
             pvoc_jsons.append(pvoc_json)
 
             if isinstance(pvoc_json['annotation']['object'], list):
@@ -43,6 +48,10 @@ for root, dirs, files in os.walk(pvoc_folder, topdown=True):
                 os.path.join(root, file_name),
                 os.path.join(sa_folder, file_name)
             )
+
+# Just in case if you want to see Pascal VOC's JSON objects converted from xml and collected in single JSON file
+# with open(os.path.join(os.path.abspath(pvoc_folder), "general_pvoc.json"), "w") as gen_pvoc_json:
+#     json.dump(pvoc_jsons, gen_pvoc_json, indent=2)
 
 sa_classes = []
 for c in list(classes):
@@ -58,55 +67,39 @@ with open(os.path.join(classes_dir, "classes.json"), "w") as classes_json:
 
 for pv_json in pvoc_jsons:
     sa_loader = []
-    if isinstance(pv_json['annotation']['object'], list):
-        for obj in pv_json['annotation']['object']:
-            try:
-                sa_bbox = {
-                        'type': 'bbox',
-                        'points':
-                                {
-                                    'x1': obj['bndbox']['xmin'],
-                                    'y1': obj['bndbox']['ymin'],
-                                    'x2': obj['bndbox']['xmax'],
-                                    'y2': obj['bndbox']['ymax']
-                                },
-                        'className': obj['name'],
-                        'classId': '?',
-                        'attributes': [{'pose': obj['pose'],
-                                        'truncated': obj['truncated'],
-                                        'difficult': obj['difficult']}],
-                        'probability': 100,
-                        'locked': False,
-                        'visible': True,
-                        'groupId': 0
-                    }
-                sa_loader.append(sa_bbox)
-            except KeyError:
-                print('KeyError')
-    else:
-        try:
-            sa_bbox = {
-                        'type': 'bbox',
-                        'points':
-                                {
-                                    'x1': pv_json['annotation']['object']['bndbox']['xmin'],
-                                    'y1': pv_json['annotation']['object']['bndbox']['ymin'],
-                                    'x2': pv_json['annotation']['object']['bndbox']['xmax'],
-                                    'y2': pv_json['annotation']['object']['bndbox']['ymax']
-                                },
-                        'className': pv_json['annotation']['object']['name'],
-                        'classId': '?',
-                        'attributes': [{'pose': pv_json['annotation']['object']['pose'],
-                                        'truncated': pv_json['annotation']['object']['truncated'],
-                                        'difficult': pv_json['annotation']['object']['difficult']}],
-                        'probability': 100,
-                        'locked': False,
-                        'visible': True,
-                        'groupId': 0
-                    }
-            sa_loader.append(sa_bbox)
-        except KeyError:
-            print("KeyError")
+    for obj in pv_json['annotation']['object']:
+
+        sa_bbox = {
+                'type': 'bbox',
+                'points':
+                        {
+                            'x1': obj['bndbox']['xmin'],
+                            'y1': obj['bndbox']['ymin'],
+                            'x2': obj['bndbox']['xmax'],
+                            'y2': obj['bndbox']['ymax']
+                        },
+                'className': obj['name'],
+                'classId': '',
+                'attributes': [],
+                'probability': 100,
+                'locked': False,
+                'visible': True,
+                'groupId': 0
+            }
+
+        if 'actions' in obj.keys():
+            for act_key, act_value in obj['actions'].items():
+                sa_bbox['attributes'].append({'id': -1, 'groupId': -2, act_key: act_value})
+
+        for key, value in obj.items():
+            if key not in ['name', 'bndbox', 'point', 'actions']:
+                sa_bbox['attributes'].append({'id': -1, 'groupId': -2, key: value})
+
+        for sa_class in sa_classes:
+            if sa_class['name'] == sa_bbox['className']:
+                sa_bbox['classId'] = sa_class['id']
+
+        sa_loader.append(sa_bbox)
 
     with open(os.path.join(sa_folder, pv_json['annotation']['filename'] + "___objects.json"), "w") as new_json:
         json.dump(sa_loader, new_json, indent=2)
